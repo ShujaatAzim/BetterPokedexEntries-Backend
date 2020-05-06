eb4d5009c5e71a1bc17e60eceacfcf79a353e1dc
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-06 00:00:35 -0400

    Got everything set up for login and auth. Learned about the cors config page.

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2020_05_02_003345) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
