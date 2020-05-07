37a2e36c298024b8dad53133f1485e8404c70d7c
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-06 21:16:48 -0400

    Login and user creation/registration works through Postman testing. Frontend requests will need to be structured. Might use separate backend without auth so that I can continue building rest of app without being logged in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_request!, except: [:create, :login]
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def login
+    user = User.find_by(username: user_params[:username].to_s.downcase)
+
+    if user && user.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode({ user_id: user.id })
+      render json: { auth_token: auth_token }, status: :ok
+    else
+      render json: { error: 'Invalid username/password' }, status: :unauthorized
+    end 
+  end
+
   def index
     @users = User.all
     render json: UsersSerializer.new(@users).to_serialized_json
@@ -11,12 +25,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save && @user.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode(user_id: @user.id)
+      render json: { auth_token: auth_token }, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def user_params
+    params.permit(:username, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
   has_many :entries
   has_many :pokemons, through: :entries
+
+  validates_presence_of :username
+  validates_uniqueness_of :username, case_sensitive: false
+
+  before_save :downcase_username
+
+  def downcase_username
+    self.username = self.username.delete(' ').downcase
+  end
 
 end
